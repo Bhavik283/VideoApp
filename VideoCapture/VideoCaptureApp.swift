e2ac5a8b916fd5ce24e708a00f2d04f329d8b8cd
Bhavik Goyal <goyal.bhavik28@gmail.com>
2025-05-13 20:30:22 +0530

    Initial Commit

---
+++ VideoCapture/VideoCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCaptureApp.swift
+//  VideoCapture
+//
+//  Created by Bhavik Goyal on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
